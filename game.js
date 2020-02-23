2309bca0642d6e8fec736043e77a16b035af0e77
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2020-02-23 18:14:37 +0530

    WIP - Simon game * added js and jquery * create a new pattern via nextSequence() * show the sequence to user with animations and sound * check which button is pressed * add sounds to button clicks

---
+++ game.js
@@ -0,0 +1,37 @@
+var buttonColors = [
+    "red", "blue", "green", "yellow"
+];
+
+var gamePattern = [
+
+];
+
+var userClickedPattern = [
+
+];
+
+
+function playSound(randomChosenColor) {
+    var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+    audio.addEventListener('canplaythrough', function () {
+        audio.play();
+    });
+}
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+
+    let $selectedColorElement = $("#"+randomChosenColor);
+    $selectedColorElement.fadeOut(50).fadeIn(50);
+    playSound(randomChosenColor);
+
+}nextSequence();
+
+//check which button is pressed
+$(".btn").click(function() {
+    let userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+});
