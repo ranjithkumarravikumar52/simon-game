15754fd6554fa848a9ec7b6ea4a9dcd83fd7dbda
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2020-02-23 19:36:47 +0530

    added gameLogic and gameOver logic

--- game.js
+++ game.js
@@ -10,6 +10,38 @@ var userClickedPattern = [
 
 ];
 
+var level = 0;
+
+/**
+ * when user clicks on any of the button
+ * 1. push the id to userClickedPattern
+ * 2. when clicks reached to gamePattern then check game logic
+ */
+$(".btn").click(function() {
+    let userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+    if(userClickedPattern.length === gamePattern.length) {
+       if(checkLogic()) {
+           console.log("Round Won");
+           userClickedPattern = [];
+           nextSequence();
+       } else {
+           console.log("Round Lost");
+           gameOver();
+       }
+    }
+});
+
+/**
+ * Start the game only when we are at level 0 and user presses any key
+ */
+$("body").keypress(function (event) {
+    if (level === 0) {
+        nextSequence();
+    }
+});
 
 function playSound(randomChosenColor) {
     var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
@@ -24,18 +56,11 @@ function nextSequence() {
     gamePattern.push(randomChosenColor);
 
     let $selectedColorElement = $("#"+randomChosenColor);
-    $selectedColorElement.fadeOut(50).fadeIn(50);
+    $selectedColorElement.fadeOut(250).fadeIn(250);
     playSound(randomChosenColor);
-
-}nextSequence();
-
-//check which button is pressed
-$(".btn").click(function() {
-    let userChosenColor = $(this).attr("id");
-    userClickedPattern.push(userChosenColor);
-    playSound(userChosenColor);
-    animatePress(userChosenColor);
-});
+    level++;
+    $("#level-title").text("Level "+ level);
+}
 
 function animatePress(currentColor) {
     let $currentColorElement = $("#"+currentColor);
@@ -43,4 +68,32 @@ function animatePress(currentColor) {
     setTimeout(function() {
         $currentColorElement.toggleClass("pressed");
     }, 100)
+}
+
+function checkLogic() {
+    console.log("=============================================================================================");
+    console.log('gamePattern ==', gamePattern);
+    console.log('userClickedPattern ==', userClickedPattern);
+
+    for (var i = 0; i < gamePattern.length; i++) {
+        if (gamePattern[i] !== userClickedPattern[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+function gameOver() {
+    $("#level-title").text("Game Over! Press any key to continue");
+    $("body").toggleClass("game-over");
+    setTimeout(function() {
+        $("body").toggleClass("game-over");
+    }, 200);
+    playSound("wrong");
+
+    level = 0;
+    userClickedPattern = [];
+    gamePattern = [];
+
 }
